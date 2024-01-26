0ad234bb70324134313fed3b2e0f30064a00175a
Kevin Hurley <kevinhurley23@gmail.com>
2024-01-26 14:22:02 -0500

    Contact section link improvements

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 export default function Contact() {
   return (
     <section id="contact" className="flex-center">
@@ -10,42 +8,18 @@ export default function Contact() {
         touch.
       </p>
       <div id="contact-buttons" className="flex-center">
-        <div className="button-and-label flex-center">
-          <div className="contact-button">
-            <a href="mailto:kevinhurley23@gmail.com" target="blank">
-              <motion.i
-                whileHover={{ scale: 1.2 }}
-                className="fa-regular fa-envelope"
-              ></motion.i>
-            </a>
-          </div>
-          <div className="contact-label">Email</div>
-        </div>
-        <div className="button-and-label flex-center">
-          <div className="contact-button">
-            <a href="https://github.com/kevinhurley23" target="blank">
-              <motion.i
-                whileHover={{ scale: 1.2 }}
-                className="fa-brands fa-github"
-              ></motion.i>
-            </a>
-          </div>
-          <div className="contact-label">GitHub</div>
-        </div>
-        <div className="button-and-label flex-center">
-          <div className="contact-button">
-            <a
-              href="https://linkedin.com/in/kevin-hurley-35a23843"
-              target="blank"
-            >
-              <motion.i
-                whileHover={{ scale: 1.2 }}
-                className="fa-brands fa-linkedin-in"
-              ></motion.i>
-            </a>
-          </div>
-          <div className="contact-label">LinkedIn</div>
-        </div>
+        <a href="mailto:kevinhurley23@gmail.com" target="blank">
+          <i className="fa-regular fa-envelope"></i>
+          <span className="contact-label">Email</span>
+        </a>
+        <a href="https://github.com/kevinhurley23" target="blank">
+          <i className="fa-brands fa-github"></i>
+          <span className="contact-label">GitHub</span>
+        </a>
+        <a href="https://linkedin.com/in/kevin-hurley-35a23843" target="blank">
+          <i className="fa-brands fa-linkedin-in"></i>
+          <span className="contact-label">LinkedIn</span>
+        </a>
       </div>
     </section>
   );
